94d2d6519442e8f4712c9d490cd2823ea7726a4d
Madilyn Barnhart <madilynbarnhart@gmail.com>
2018-11-07 08:19:02 -0600

    Denial
    Deny the crime

--- team4.py
+++ team4.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'MB_AC_ME' # Only 10 chars displayed.
+strategy_name = 'Denial'
+strategy_description = 'If the partner in crime colludes more often than betraying, we will collude'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
