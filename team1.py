5e7da8fb17cb848eadd12237150140ca173471c7
stounat <nathaniel.stout0112@gmail.com>
2018-11-13 08:02:32 -0600

    Team 1 Program
    Team 1 Program

--- team1.py
+++ team1.py
@@ -1,3 +1,4 @@
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -9,7 +10,7 @@
 team_name = 'Jacob?' # Only 10 chars displayed.
 strategy_name = 'No_Idea_Yet'
 strategy_description = 'In_The_Process_Of'
-    
+
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
@@ -17,6 +18,24 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
+    Time = 0
+    if len(my_history)==0:
+        Time = 8
+    else:
+        Time = random.randint(1,24)
+    if len(my_history) == 0:
+        return 'b'
+    else:
+        if Time > 12:
+            return 'b'
+        elif their_history[len(their_history)-1] == 'b':
+            return 'b'
+        elif my_score < their_score + 150:
+            return 'b'
+        else:
+            return 'c'
+        
+            
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -48,11 +67,11 @@ def test_move(my_history, their_history, my_score, their_score, result):
 if __name__ == '__main__':
      
     # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
-              my_score=0,
+    if test_move(my_history='bbc',
+              their_history='bbc', 
+              my_score=155,
               their_score=0,
-              result='b'):
+              result='c'):
          print 'Test passed'
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
