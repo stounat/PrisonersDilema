e2f28bfd1267bce413548f18a42145fa07e97432
stounat <nathaniel.stout0112@gmail.com>
2018-11-13 08:43:01 -0600

    Team 1
    New names

--- team1.py
+++ team1.py
@@ -1,3 +1,91 @@
+<<<<<<< HEAD
+import random
+####
+# Each team's file must define four tokens:
+#     team_name: a string
+#     strategy_name: a string
+#     strategy_description: a string
+#     move: A function that returns 'c' or 'b'
+####
+
+team_name = 'Jacob?' # Only 10 chars displayed.
+strategy_name = 'Operation Charlie is a Jerk'
+strategy_description = 'Time based betrayal'
+
+def move(my_history, their_history, my_score, their_score):
+    ''' Arguments accepted: my_history, their_history are strings.
+    my_score, their_score are ints.
+    
+    Make my move.
+    Returns 'c' or 'b'. 
+    '''
+    Time = 0
+    if len(my_history)==0:
+        Time = 8
+    else:
+        Time = random.randint(1,24)
+    if len(my_history) == 0:
+        return 'b'
+    else:
+        if Time > 12:
+            return 'b'
+        elif their_history[len(their_history)-1] == 'b':
+            return 'b'
+        elif my_score < their_score + 150:
+            return 'b'
+        else:
+            return 'c'
+        
+            
+
+    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
+    # their_history: a string of the same length as history, possibly empty. 
+    # The first round between these two players is my_history[0] and their_history[0].
+    # The most recent round is my_history[-1] and their_history[-1].
+    
+    # Analyze my_history and their_history and/or my_score and their_score.
+    # Decide whether to return 'c' or 'b'.
+    
+    return 'c'
+
+    
+def test_move(my_history, their_history, my_score, their_score, result):
+    '''calls move(my_history, their_history, my_score, their_score)
+    from this module. Prints error if return value != result.
+    Returns True or False, dpending on whether result was as expected.
+    '''
+    real_result = move(my_history, their_history, my_score, their_score)
+    if real_result == result:
+        return True
+    else:
+        print("move(" +
+            ", ".join(["'"+my_history+"'", "'"+their_history+"'",
+                       str(my_score), str(their_score)])+
+            ") returned " + "'" + real_result + "'" +
+            " and should have returned '" + result + "'")
+        return False
+
+if __name__ == '__main__':
+     
+    # Test 1: Betray on first move.
+    if test_move(my_history='bbc',
+              their_history='bbc', 
+              my_score=155,
+              their_score=0,
+              result='c'):
+         print 'Test passed'
+     # Test 2: Continue betraying if they collude despite being betrayed.
+    test_move(my_history='bbb',
+              their_history='ccc', 
+              # Note the scores are for testing move().
+              # The history and scores don't need to match unless
+              # that is relevant to the test of move(). Here,
+              # the simulation (if working correctly) would have awarded 
+              # 300 to me and -750 to them. This test will pass if and only if
+              # move('bbb', 'ccc', 0, 0) returns 'b'.
+              my_score=0, 
+              their_score=0,
+=======
 import random
 ####
 # Each team's file must define four tokens:
@@ -84,4 +172,5 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
+>>>>>>> 930e66048f780f03db842a40ca28b2107db70839
               result='b')             
